747ef96a9203c45435f219d54f70816e51920f08
Shubham Keshri <shubhamkeshrikgg@gmail.com>
2024-12-25 14:30:53 +0530

    Download all dependencies and set env variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 export default function App() {
+  
+  console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
+    
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
