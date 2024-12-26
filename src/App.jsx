f61b8a5d1b35b25842524bfca571b624944263c4
Shubham Keshri <shubhamkeshrikgg@gmail.com>
2024-12-26 22:19:52 +0530

    Configure the redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { useState,useEffect } from "react";
+import { Header, Footer } from "./components";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./features/authSlice";
+
 export default function App() {
-  
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block m-2 ">
+        <Header />
+        <main>
+          ToDO{/* <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
